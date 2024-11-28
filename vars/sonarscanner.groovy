d6dad77eaea92a33dd8b3cd3b38e2dd8b99ddb4f
Vidhya Shanker Goel <68071830+vidhya101@users.noreply.github.com>
2024-11-28 13:42:14 -0500

    Update sonarscanner.groovy

--- vars/sonarscanner.groovy
+++ vars/sonarscanner.groovy
@@ -1,15 +1,19 @@
-def call() {
-withSonarQubeEnv('sonarserver') {
-                    sh """
-                        export SONAR_SCANNER_OPTS="${JAVA_OPTS}"
-                        ${scannerHome}/bin/sonar-scanner \
-                        -Dsonar.projectKey=vprofile \
-                        -Dsonar.projectName=vprofile-repo \
-                        -Dsonar.projectVersion=1.0 \
-                        -Dsonar.sources=src/ \
-                        -Dsonar.java.binaries=target/test-classes/com/visualpathit/account/controllerTest/ \
-                        -Dsonar.junit.reportsPath=target/surefire-reports/ \
-                        -Dsonar.jacoco.reportsPath=target/jacoco.exec \
-                        -Dsonar.java.checkstyle.reportPaths=target/checkstyle-result.xml
-                    """
+def call(Map config = [:]) {
+    def scannerHome = tool 'sonar6.2'
+    def javaOpts = '-XX:+EnableDynamicAgentLoading --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.math=ALL-UNNAMED'
+    
+    withSonarQubeEnv('sonarserver') {
+        sh """
+            export SONAR_SCANNER_OPTS="${javaOpts}"
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${config.projectKey ?: 'default-project'} \
+            -Dsonar.projectName=${config.projectName ?: 'default-project-name'} \
+            -Dsonar.projectVersion=${config.projectVersion ?: '1.0'} \
+            -Dsonar.sources=${config.sources ?: 'src/'} \
+            -Dsonar.java.binaries=${config.binaries ?: 'target/test-classes/'} \
+            -Dsonar.junit.reportsPath=${config.junitReports ?: 'target/surefire-reports/'} \
+            -Dsonar.jacoco.reportsPath=${config.jacocoReports ?: 'target/jacoco.exec'} \
+            -Dsonar.java.checkstyle.reportPaths=${config.checkstyleReports ?: 'target/checkstyle-result.xml'}
+        """
+    }
 }
