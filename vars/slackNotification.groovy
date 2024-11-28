17443d8ca03a0256ecb3dece0b7591566581a96f
Vidhya Shanker Goel <68071830+vidhya101@users.noreply.github.com>
2024-11-28 13:46:02 -0500

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,1 +1,12 @@
-
+def call(Map config = [:]) {
+    def COLOR_MAP = [
+        'SUCCESS': 'good', 
+        'FAILURE': 'danger',
+    ]
+    
+    slackSend(
+        channel: config.channel ?: '#devops-ci',
+        color: COLOR_MAP[currentBuild.currentResult],
+        message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+    )
+}
