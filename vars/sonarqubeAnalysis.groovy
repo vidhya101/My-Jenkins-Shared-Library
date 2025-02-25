419047b45087a9c33f7f91c60cd7e2e031bd55e1
Vidhya Shanker Goel <68071830+vidhya101@users.noreply.github.com>
2025-02-25 12:22:59 -0500

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,12 +1,12 @@
-def call() {
-    withSonarQubeEnv('sonarserver') {
-        sh ''' 
-        export SONAR_SCANNER_OPTS="${JAVA_OPTS}"
-        $SCANNER_HOME/bin/sonar-scanner 
-        -Dsonar.projectName=Youtube 
-        -Dsonar.projectKey=Youtube 
-        -Dsonar.projectVersion=1.0 
-        -Dsonar.sources=src
-        '''
+def call(String sonarServer, String scannerHome) {
+    withSonarQubeEnv(sonarServer) {
+        sh """
+            export SONAR_SCANNER_OPTS="${JAVA_OPTS}"
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=Youtube \
+            -Dsonar.projectName=Youtube \
+            -Dsonar.projectVersion=1.0 \
+            -Dsonar.sources=src/
+        """
     }
 }
