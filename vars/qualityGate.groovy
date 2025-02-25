96bfd453a6cee4ebec5c2fb983f8132fae319e72
Vidhya Shanker Goel <68071830+vidhya101@users.noreply.github.com>
2025-02-25 14:33:19 -0500

    Update qualityGate.groovy

--- vars/qualityGate.groovy
+++ vars/qualityGate.groovy
@@ -1,3 +1,8 @@
 def call(credentialsId) {
-    waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+    def qualityGate = waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+    if (qualityGate.status != 'OK') {
+        error "❌ Quality Gate failed: ${qualityGate.status}"
+    } else {
+        echo "✅ Quality Gate passed!"
+    }
 }
