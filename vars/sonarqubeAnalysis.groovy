94a29285c9071c9e95337f9ef9fc344120e2395f
Vidhya Shanker Goel <68071830+vidhya101@users.noreply.github.com>
2025-02-25 14:32:56 -0500

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,13 @@
 def call() {
     withSonarQubeEnv('sonarserver') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube '''
+        sh '''
+            ${SCANNER_HOME}/bin/sonar-scanner \
+            -Dsonar.projectName=Youtube \
+            -Dsonar.projectKey=Youtube \
+            -Dsonar.sources=. \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.exclusions=**/node_modules/**,**/test/** \
+            -Dsonar.verbose=true
+        '''
     }
 }
